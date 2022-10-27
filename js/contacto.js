a2fff01b423e54a83318a208d35a07ff7764b043
dri-udi <114609810+dri-udi@users.noreply.github.com>
2022-10-27 13:11:16 -0500

    prueba

--- js/contacto.js
+++ js/contacto.js
@@ -1,17 +1,17 @@
-const formCisco = document.forms['formXamai'];
-const scriptURLXamai = "https://script.google.com/macros/s/AKfycbw3UdnCxHq8FgqNOsUdq_UQH9RwfFO5BYRkpzEls9ILk1332JAbwGsfUopTLPLgKOWp/exec";
-let Nombre = document.getElementById("Nombre");
-let UA = document.getElementById("UA/CI");
-let Correo = document.getElementById("Correo");
-let Comentario = document.getElementById("dudas");
+const formXamai = document.forms['formXamai'];
+const scriptURLXamai = "https://script.google.com/macros/s/AKfycbyNa921I7o-8nxFFnVGEB9iGVrhnzrB8vJbKnq0mh-k9lHfiGNIrO-Vp79KqpTbPbXeiw/exec";
+
 var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
     keyboard: false
 })
 
-formCisco.addEventListener('submit', e => {
+
+formXamai.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURLXamai, {
+        
         method: 'POST',
+       
         body: new FormData(formXamai)
     })
     .then(response => Cambio())
