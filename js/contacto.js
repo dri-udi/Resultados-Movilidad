4da8eccbb40233cc748456574246c42b01769864
dri-udi <114609810+dri-udi@users.noreply.github.com>
2022-10-27 13:44:53 -0500

    prueba

--- js/contacto.js
+++ js/contacto.js
@@ -4,6 +4,34 @@ const scriptURLXamai = "https://script.google.com/macros/s/AKfycbyNa921I7o-8nxFF
 var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
     keyboard: false
 })
+var CLIENT_ID = '471138515780-20m05f7glrkmn016vh9aca9v18tg9upq.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyB319n1GzvpuWWCXIcwAl8-iQU_JQchv7Q';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function(error) {
+        console.log(JSON.stringify(error, null, 2));
+    });
+}
 
 
 formXamai.addEventListener('submit', e => {
